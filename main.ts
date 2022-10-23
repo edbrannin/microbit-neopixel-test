4214bc20872236c2e53d50c37c726443ea0b17cd
Ed Brannin <edbrannin@gmail.com>
2022-10-23 09:26:49 -0400

    Double Rainbow!

--- main.ts
+++ main.ts
@@ -1,19 +1,39 @@
 function Update () {
-    strip.rotate(1)
+    RainbowStart = ShiftColor(Step)
+    Left.showRainbow(RainbowStart, RainbowStart + range)
+    Right.showRainbow(ShiftColor(EarOffset), ShiftColor(EarOffset + range))
     strip.show()
 }
+input.onButtonPressed(Button.A, function () {
+    Step += 1
+})
+function ShiftColor (num: number) {
+    return (RainbowStart + num) % 360
+}
+input.onButtonPressed(Button.B, function () {
+    Step += -1
+})
+let Left: neopixel.Strip = null
+let Right: neopixel.Strip = null
 let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P2, 8, NeoPixelMode.RGB)
-let Step = 1
-let Hue = 0
-strip.showRainbow(1, 360)
-basic.pause(1000)
+let EarOffset = 0
+let Step = 0
+let RainbowStart = 0
+let range = 0
+let frameDelay = 5
+range = 10
+RainbowStart = 1
+Step = 1
+EarOffset = 180
+strip = neopixel.create(DigitalPin.P2, 16, NeoPixelMode.RGB)
+Right = strip.range(0, 8)
+Left = strip.range(8, 8)
 Update()
 basic.forever(function () {
     Update()
     basic.showIcon(IconNames.Square)
-    basic.pause(100)
+    basic.pause(frameDelay)
     Update()
     basic.showIcon(IconNames.SmallSquare)
-    basic.pause(100)
+    basic.pause(frameDelay)
 })
